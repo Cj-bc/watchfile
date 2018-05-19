3130de0373d646b7fe6f3ad9e2b4ec937c48f606
Cj-bc <cj-bc@y-modify.org>
2018-05-20 08:05:33 +0900

    Add: watchfile> complete color support for todo.txt

--- watchfile.sh
+++ watchfile.sh
@@ -8,27 +8,45 @@
 
 watched=$1
 
-COLOR_YELLOW='\\033[1;33m'
-COLOR_DEFAULT='\\033[0m'
+COLOR_YELLOW=3
+COLOR_GREEN=2
+COLOR_BLUE=4
+COLOR_WHITE=7
 
-# echo files with color scheme (if there's configuration here)
+
+# get todo.txt's TODO_FILE and return file name
+# @return 0 succcess
+function watchfile.todofilename {
+  file_path=$(awk -F "\"" '/TODO_FILE/ {print $2}' ~/.todo.cfg)
+  echo ${file_path##*\/}
+  return 0
+}
+
+
+
+# echo files with color for each line(if there's configuration here)
 # @param <string file_name>
 # @
 function watchfile.colcat {
+
+  # detect file type and chose which color to add.
   case $1 in
-    "todo/todo.txt" )
-      while read line; do
-        local echoed=($line)
-        # color scheme for priority
-        case ${echoed[0]} in
-          "(A)" ) tput setaf 3;;
-          "(B)" ) tput setaf 2;;
-          * ):;;
-        esac
-        echo $line
-        tput sgr0
-      done < $1
-      ;;
+    *$(watchfile.todofilename) )
+      # for each line, detect priority and decide color.
+        while read line; do
+          local words=($line)
+          case ${words[0]} in
+            "(A)" ) tput setaf $COLOR_YELLOW;;
+            "(B)" ) tput setaf $COLOR_GREEN;;
+            "(C)" ) tput setaf $COLOR_BLUE;;
+            "(X)" ) tput setaf $COLOR_WHITE;;
+            * ):;;
+          esac
+          echo $line
+          tput sgr0
+        done < $1
+        ;;
+    * ) cat $1
   esac
 }
 
