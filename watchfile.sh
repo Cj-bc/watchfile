21de2afe1111c1dbe2c850058b0d9ef437bf7eb7
Cj-bc <cj-bc@y-modify.org>
2018-05-20 09:08:12 +0900

    Fix: comments> add some comments

--- watchfile.sh
+++ watchfile.sh
@@ -1,4 +1,5 @@
 #!/usr/local/bin/bash
+#
 # watchfile - watch file and display them & update contents
 #
 # ver: 0.0.1
@@ -50,13 +51,16 @@ function watchfile.colcat {
   esac
 }
 
+
+# 1. get footprint sha256
+# 2. if it's changed, clear the screen and cat the file
+# 3. update the footprint
 while true; do
   new_sha256=$(openssl sha256 -r $watched | awk '{print $1}')
   if [ "$new_sha256" != "$pre_sha256" ]; then
     clear
     tput cup 1 1
     watchfile.colcat $watched
-    echo "---"; echo TODO
     pre_sha256=$new_sha256
   fi
   sleep 0.5
